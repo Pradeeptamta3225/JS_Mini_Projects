7ea743a7a16b1e700c4c51f9434c6a8a392a6049
Pradeep Tamta <118155647+Pradeeptamta3225@users.noreply.github.com>
2024-05-03 17:38:09 +0000

    practice

--- scripts.js
+++ scripts.js
@@ -1,30 +1,36 @@
-const countValue = document.querySelector("#count");
-// logic 
-const increment = () => {
-    // get update 
-    let value = parseInt(countValue.innerText);
-    // what the update 
-    value = value + 1;
-    // value what the display 
-    countValue.innerText = value;
-}
+// const countValue = document.querySelector("#count");
+// // logic 
+// const increment = () => {
+//     // get update 
+//     let value = parseInt(countValue.innerText);
+//     // what the update 
+//     value = value + 1;
+//     // value what the display 
+//     countValue.innerText = value;
+// }
 
-const decrement = () => {
-   // get update 
-   let value = parseInt(countValue.innerText);
-   // what the update 
-   value = value - 1;
-   // value what the display 
-   countValue.innerText = value;
-}
-
-
-const reset = () =>{
-     value = 0;
-     countValue.innerText = value;
-}
+// const decrement = () => {
+//    // get update 
+//    let value = parseInt(countValue.innerText);
+//    // what the update 
+//    value = value - 1;
+//    // value what the display 
+//    countValue.innerText = value;
+// }
 
 
-//Hello
+// const reset = () =>{
+//      value = 0;
+//      countValue.innerText = value;
+// }
 
-console.log(..."Pradeeep");
+
+// //Hello
+
+// console.log(..."Pradeeep");
+
+
+
+console.log(NaN === NaN);
+
+console.log(2 + [2] + ({} + []));
